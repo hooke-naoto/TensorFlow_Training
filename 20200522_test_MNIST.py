99f8772aa4b30f73c4939a6a5d4fccd656c3452c
田中直人 <naoto40@gmail.com>
2020-05-23 15:30:30 +0900

    update

--- 20200522_test_MNIST.py
+++ 20200522_test_MNIST.py
@@ -1,5 +1,14 @@
 import tensorflow as tf
 mnist = tf.keras.datasets.mnist
+
+import argparse
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--epochs_num', help='Epoch number to fit the model.', required=False, type=int, default=5)
+parser.add_argument('--batch_size_num', help='Batch size to fit the model.', required=False, type=int, default=128)
+args = parser.parse_args()
+epochs_num = args.epochs_num
+batch_size_num = args.batch_size_num
+
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 train_images = train_images / 255.0
 test_images = test_images / 255.0
@@ -12,7 +21,7 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10, activation='softmax')
 ])
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-print('[        model.fit        ]')
-model.fit(train_images, train_labels, epochs=5, batch_size=1024)
-print('[        model.evaluate        ]')
+print('-------- model.fit --------')
+model.fit(train_images, train_labels, epochs=epochs_num, batch_size=batch_size_num)
+print('-------- model.evaluate --------')
 test_loss, test_acc = model.evaluate(test_images, test_labels)
